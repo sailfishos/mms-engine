7a7002e58fed2a465785110735dbed05d5a9c611
Slava Monich <slava.monich@jolla.com>
2018-03-12 15:16:03 +0300

    [mms-lib] Fixed build against libglibutil >= 1.0.27. MER#1437

--- mms-lib/include/mms_lib_types.h
+++ mms-lib/include/mms_lib_types.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013-2016 Jolla Ltd.
+ * Copyright (C) 2013-2018 Jolla Ltd.
  * Contact: Slava Monich <slava.monich@jolla.com>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -32,6 +32,8 @@
 #include <fcntl.h>
 
 #include <gutil_types.h>
+
+#include <glib-object.h>
 
 typedef GLogModule MMSLogModule;
 
