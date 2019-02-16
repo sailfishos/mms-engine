2404b679d7e1fcbb8758cd2035d71eae72ab0f4b
Slava Monich <slava.monich@jolla.com>
2019-02-16 14:51:24 +0300

    Version 1.0.64

--- mms-engine/mms_version.h
+++ mms-engine/mms_version.h
@@ -16,7 +16,7 @@
 #define JOLLA_MMS_VERSION_H
 
 #ifndef MMS_ENGINE_VERSION
-#  define MMS_ENGINE_VERSION 1.0.63
+#  define MMS_ENGINE_VERSION 1.0.64
 #endif
 
 #define MMS_VERSION_STRING__(x) #x
--- rpm/mms-engine.spec
+++ rpm/mms-engine.spec
@@ -1,6 +1,6 @@
 Name:     mms-engine
 Summary:  MMS engine
-Version:  1.0.63
+Version:  1.0.64
 Release:  1
 Group:    Communications/Telephony and IM
 License:  GPLv2
